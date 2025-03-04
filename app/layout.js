7fe4125e8cc5714d0f4d8f5ab95bcffaf76b2a48
Chandra Prayogo <chandrathen28@gmail.com>
2025-03-04 17:22:12 +0700

    update wording

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ import "./css/globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of Chandra Prayogo - Software Developer",
+  title: "Portfolio of Chandra Prayogo - Software Engineer",
   description:
     "This is the portfolio of Chandra Prayogo. I am a backend engineer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
